193d39f2547a8c7ae1d01e8f3d706f3e682eccec
ting <bullian99@gmail.com>
2022-12-11 22:57:28 +0800

    add android/x86 to build script

--- build-android.sh
+++ build-android.sh
@@ -12,3 +12,11 @@ CGO_ENABLED=1 \
     go build
 
 zip mixch-dl-android-arm64.zip mixch-dl
+
+#CGO_ENABLED=1 \
+#    GOOS=android \
+#    CC=$NDK/bin/x86_64-linux-android24-clang \
+#    CXX=$NDK/bin/x86_64-linux-android24-clang \
+#    go build
+#
+#zip mixch-dl-android-x86_64.zip mixch-dl
